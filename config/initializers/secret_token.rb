11b4da3ff34b0e092bf3c35eb046f3129d9b794b
Ben Hu <benzheren@gmail.com>
2011-05-14 00:27:08 -0700

    generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ziccos::Application.config.secret_token = 'e5869831d56ee8e7cd0c4b825052bf20eca802dea4e08174cff03d41fc0123fbe4231a110398d3c12de4ae2f154d214bcc75bb9d3810436e21bb1b46d97e2854'
