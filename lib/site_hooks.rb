110ff8c7e6da0c8ca44743899a236b199a5d8593
Ben Hu <benzheren@gmail.com>
2011-05-18 21:52:25 -0700

    add first hook locale selector, ugly but works

--- lib/site_hooks.rb
+++ lib/site_hooks.rb
@@ -1,5 +1,3 @@
 class SiteHooks < Spree::ThemeSupport::HookListener
-  insert_after :shared_login_bar do
-    #'<p><%= link_to "en", :controller => "locale", :action => "set", :locale => "en" %></p>'
-  end
+  insert_after :shared_login_bar, 'shared/locale_selector'
 end
