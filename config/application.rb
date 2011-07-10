5a6b53c4951c6296532d7d071c77cc1f565559d7
Ben Hu <benzheren@gmail.com>
2011-07-09 23:13:22 -0700

    try to fix deploy problem

--- config/application.rb
+++ config/application.rb
@@ -31,11 +31,11 @@ module Ziccos
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :"zh-CN"
+    # config.i18n.default_locale = :"zh-CN"
     
-    config.after_initialize do 
-       Spree::Config.set(:default_locale => config.i18n.default_locale) 
-     end 
+    # config.after_initialize do 
+    #   Spree::Config.set(:default_locale => config.i18n.default_locale) 
+    # end 
     
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
